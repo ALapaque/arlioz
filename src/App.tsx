1b376e0a06e0129358a03d32c1021c002b155bdd
amaury <amaury.lapaque@gmail.com>
2023-12-29 19:55:34 +0100

    initial website state, with coming soon page

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import arliozLogo from './assets/arlioz.svg'
+
+
+function App() {
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col">
+					<div className="comming-soon">
+							<img className={'logo'} src={arliozLogo} alt={'arlioz-logo'}/>
+						<div className="coming-soon-container">
+							<p className="coming-soon-container_text">COMMING SOON</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+)
+}
+
+export default App
